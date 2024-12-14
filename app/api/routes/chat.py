958a0b03136b0972a0933352104a6188ba31cea3
Nikola Nikolovski <nikolovski.nikola42@gmail.com>
2024-12-14 18:45:05 +0100

    Update dependencies and add new chat API endpoints
    Upgraded certifi, langchain-core, and uvicorn dependencies to their latest versions. Added two new endpoints: one for retrieving chat messages (`/get_chat_messages/{chat_id}`) and another for updating chats (`/update_chat/{chat_id}`). Introduced `MessagesDto` for handling structured message data.

--- app/api/routes/chat.py
+++ app/api/routes/chat.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
-from typing import Annotated
+from typing import Annotated, Tuple, List
 
+from bson import ObjectId
 from fastapi import HTTPException, APIRouter, Depends
 from pydantic import BaseModel
 
@@ -30,6 +31,19 @@ async def get_chats(mdb: db_dep):
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Failed to add entry")
     return chats
 
+@router.get("/get_chat_messages/{chat_id}", status_code=HTTPStatus.CREATED)
+async def get_chat_messages(chat_id:str ,mdb: db_dep):
+    try:
+        user_messages = await mdb.get_entries(Message, doc_filter={"chat_id": chat_id, "role": "user"})
+        assistant_messages = await mdb.get_entries(Message, doc_filter={"chat_id": chat_id, "role": "assistant"})
+
+        user_messages = sorted(user_messages, key=lambda x: x.order)
+        assistant_messages = sorted(assistant_messages, key=lambda x: x.order)
+    except Exception as e:
+        logging.error(f"Failed to add entry: {e}")
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Failed to add entry")
+    return {"user_messages": user_messages, "assistant_messages": assistant_messages}
+
 
 @router.post("/add_chat/", status_code=HTTPStatus.CREATED)
 async def add_chat(chat_dto: ChatDto, mdb: db_dep):
@@ -55,6 +69,35 @@ async def add_chat(chat_dto: ChatDto, mdb: db_dep):
                 content=message,
                 order= i,
                 chat_id=chat_obj_id
+            ))
+
+    except Exception as e:
+        logging.error(f"Failed to add entry: {e}")
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Failed to add entry")
+    return True
+
+class MessagesDto(BaseModel):
+    user_messages: List[Tuple[str, int]]
+    assistant_messages: List[Tuple[str, int]]
+
+
+@router.post("/update_chat/{chat_id}", status_code=HTTPStatus.CREATED)
+async def update_chat(chat_id: str ,messages_dto: MessagesDto, mdb: db_dep):
+    try:
+        for i,tup in enumerate(messages_dto.user_messages):
+            await mdb.add_entry(Message(
+                role="user",
+                content=tup[0],
+                order= tup[1],
+                chat_id=chat_id
+            ))
+
+        for i,tup in enumerate(messages_dto.assistant_messages):
+            await mdb.add_entry(Message(
+                role="assistant",
+                content=tup[0],
+                order=tup[1],
+                chat_id=chat_id
             ))
 
     except Exception as e:
